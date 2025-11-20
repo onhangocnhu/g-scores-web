fa7117708d541107d349515d3d7faa004575631d
onhangocnhu <nhu.onhangoc@hcmut.edu.vn>
2025-11-20 14:29:48 +0000

    initial commit

---
+++ g-scores-frontend/src/App.tsx
@@ -0,0 +1,13 @@
+// import { useState } from 'react'
+import './App.css'
+import Header from './layouts/HeaderLayout.tsx'
+
+function App() {
+  return (
+    <>
+      <Header></Header>
+    </>
+  )
+}
+
+export default App
